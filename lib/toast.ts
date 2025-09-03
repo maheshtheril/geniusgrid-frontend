0d551d8c11f48d69a3affae92b95f6017803553b
Mahesh Theril <maheshtheril@live.com>
2025-09-03 08:43:40 +0530

    fix: unify toast notify function

--- lib/toast.ts
+++ lib/toast.ts
@@ -1,13 +1,9 @@
-import { toast } from "react-hot-toast";
+import { toast } from "react-hot-toast"
 
-export function notify(message: string) {
-  toast(message);
-}
-
-export function notifyError(message: string) {
-  toast.error(message);
-}
-
-export function notifySuccess(message: string) {
-  toast.success(message);
+export function notify(message: string, type: "success" | "error" = "success") {
+  if (type === "error") {
+    toast.error(message)
+  } else {
+    toast.success(message)
+  }
 }
