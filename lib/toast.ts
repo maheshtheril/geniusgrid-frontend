60054a250ee2cf99f7c1687cd72a929cfc8e4495
Mahesh Theril <maheshtheril@live.com>
2025-09-03 10:26:14 +0530

    fix: make notify support 2 args

--- lib/toast.ts
+++ lib/toast.ts
@@ -1,7 +1,11 @@
 import { toast } from "react-hot-toast"
 
-export function notify(message: string) {
-  toast.success(message)
+export function notify(message: string, type: "success" | "error" = "success") {
+  if (type === "error") {
+    toast.error(message)
+  } else {
+    toast.success(message)
+  }
 }
 
 export function notifyError(message: string) {
