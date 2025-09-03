feee6ceba4c6f5a413910f30341785d9fe4b4192
Mahesh Theril <maheshtheril@live.com>
2025-09-03 10:17:52 +0530

    update: latest changes

--- lib/toast.ts
+++ lib/toast.ts
@@ -1,15 +1,9 @@
 import { toast } from "react-hot-toast"
 
-// Flexible notify. (1 or 2 args)
-export function notify(message: string, type: "success" | "error" = "success") {
-  if (type === "error") {
-    toast.error(message)
-  } else {
-    toast.success(message)
-  }
+export function notify(message: string) {
+  toast.success(message)
 }
 
-// For legacy imports
 export function notifyError(message: string) {
   toast.error(message)
 }
